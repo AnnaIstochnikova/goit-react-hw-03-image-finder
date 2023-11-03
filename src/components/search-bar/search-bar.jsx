9ca84fdf9dc6375bdf389dced2e9ddf53d970ca4
AnnaIstochnikova <anya.istochnikova@gmail.com>
2023-11-03 12:35:45 +0100

    Move submit button to separate file

--- src/components/search-bar/search-bar.jsx
+++ src/components/search-bar/search-bar.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 
+import { SubmitBtn } from 'components/submit-btn/submit-btn';
+
 export const Searchbar = ({ fnOnFormSubmit }) => {
   const handleSubmit = event => {
     fnOnFormSubmit(event);
@@ -7,14 +9,7 @@ export const Searchbar = ({ fnOnFormSubmit }) => {
   return (
     <header className="searchbar">
       <form className="form" onSubmit={handleSubmit}>
-        <button type="submit" className="button">
-          <img
-            //style={{ height: '100%', width: '100%' }}
-            srcSet={`${process.env.PUBLIC_URL}/search.svg`}
-            alt="Icon search"
-          />
-        </button>
-
+        <SubmitBtn />
         <input
           id="input"
           className="input"
